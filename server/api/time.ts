03be23452d1f2735a8a81c8832cb0253be6c2e3d
cfabrice <fab@wonderweb.ch>
2025-02-18 17:13:44 +0100

    init

---
+++ server/api/time.ts
@@ -0,0 +1,17 @@
+export default defineCachedEventHandler(
+  async (event) => {
+    const time = new Date();
+    return time.toLocaleString();
+  },
+  {
+    maxAge: 30, // 30 seconds
+    swr: true, // Default cache options
+    group: "time",
+    name: "time",
+    getKey: () => "time",
+    shouldBypassCache: (event) => {
+      const { preview } = getQuery(event);
+      return preview === "true";
+    },
+  }
+);
