c7aeff53b89b79c33ef6b4b82e49f4372ffc4d30
cfabrice <fab@wonderweb.ch>
2025-02-19 13:58:47 +0100

    test again

--- server/api/time.ts
+++ server/api/time.ts
@@ -1,5 +1,12 @@
 export default defineCachedEventHandler(
   async (event) => {
+    setResponseHeader(
+      event,
+      "Cache-Control",
+      "no-store, no-cache, must-revalidate, proxy-revalidate"
+    );
+    setResponseHeader(event, "Pragma", "no-cache");
+    setResponseHeader(event, "Expires", "0");
     const time = new Date();
     return time.toLocaleString();
   },
